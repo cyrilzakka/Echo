5e7fa6865e392b53c5e4d90f07be0d6c4a02e033
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2024-10-12 11:05:29 -0700

    Initial Commit

---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Cyril Zakka on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
