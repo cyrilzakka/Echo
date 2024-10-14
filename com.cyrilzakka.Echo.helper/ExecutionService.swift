7bb2cef1004cd49e47894df028a1e955b35c84e9
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2024-10-14 13:21:24 -0700

    Cleanup

--- com.cyrilzakka.Echo.helper/ExecutionService.swift
+++ com.cyrilzakka.Echo.helper/ExecutionService.swift
@@ -9,36 +9,6 @@ import Foundation
 
 enum ExecutionService {
     
-    static func executePowerMetrics() async throws -> String {
-        let process = Process()
-        process.executableURL = URL(fileURLWithPath: "/usr/bin/powermetrics")
-        process.arguments = [
-            "-s", "cpu_power,gpu_power,thermal,network,disk",
-            "--show-process-gpu",
-            "--show-process-energy",
-            "--show-initial-usage",
-            "--show-process-netstats",
-            "-i", "1000",
-            
-        ]
-        
-        let outputPipe = Pipe()
-        process.standardOutput = outputPipe
-        process.standardError = outputPipe
-        try process.run()
-        
-        return try await Task {
-            let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-            
-            guard let output = String(data: outputData, encoding: .utf8) else {
-                throw EchoError.invalidStringConversion
-            }
-            
-            return output
-        }
-        .value
-    }
-    
     static func streamPowerMetrics(updateHandler: @escaping (String) -> Void) -> Process {
         NSLog("🟠Starting powermetrics streaming")
         let process = Process()
